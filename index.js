752f322811062599610b8a1da786355c6be6330b
Alex <alexandrucioi@gmail.com>
2022-08-09 17:07:37 +0300

    1st commit

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ class Cart {
     }
 
     addProduct(id, quantity) {
-       // this.productList.push()
         if (this.productList.has(id))
             this.productList.set(id, this.productList.get(id) + quantity);
         else
@@ -13,6 +12,38 @@ class Cart {
 
     deleteProduct(id) {
         this.productList.delete(id);
+    }
+
+    setAddress(address) {
+        this.address = address;
+    }
+
+    setLivrare(livrare) {
+        this.livrare = livrare;
+    }
+
+    setFacturare(facturare) {
+        this.facturare = facturare;
+    }
+
+    setPlata(plata) {
+        this.plata = plata;
+    }
+
+    getAddress() {
+        return this.address;
+    }
+
+    getLivrare() {
+        return this.livrare;
+    }
+
+    getFacturare() {
+        return this.facturare;
+    }
+
+    getPlata() {
+        return this.plata;
     }
 }
 
@@ -25,24 +56,58 @@ class Product {
 }
 
 class Address {
-    constructor (adresa) {
-        this.adresa = adresa;
-    }
+    constructor() { }
 }
 
 class Livrare {
-    constructor (adresa) {
-        this.livrare = adresa;
-    }
+    constructor() { }
 }
 
 class Facturare {
-    constructor ()
+    constructor() { }
+}
+
+class Plata {
+    constructor() { }
 }
 
 cos = new Cart();
+adresa = new Address();
+livrare = new Livrare();
+factura1 = new Facturare();
+factura2 = new Facturare();
+plata1 = new Plata();
+
+cos.setAddress(adresa);
+
 cos.addProduct(5, 2);
 cos.addProduct(5, 3);
 cos.addProduct(2, 1);
 cos.deleteProduct(5);
+
+adresa.judet = 'Dolj';
+adresa.oras = 'Craiova';
+adresa.strada = 'Sfintii Apostoli';
+
+livrare.destinatie = adresa;
+
+factura1.CNP = '12345678';
+factura1.adresaFacturare = adresa;
+
+factura2.CUI = '6859662';
+factura2.adresaFacturare = adresa;
+
+plata1.tip = 'Card';
+plata1.numarCard = '4469 5350 2910 6378';
+plata1.lunaExpirare = '08';
+plata1.anExpirare = '25';
+plata1.numeDetinator = 'Mihai Alexandru'
+plata1.cvv = '327';
+
 console.log(cos);
+
+/*console.log(livrare);
+console.log(adresa);
+console.log(factura1);
+console.log(factura2);
+console.log(plata1); */
