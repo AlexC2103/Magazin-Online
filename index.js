3316024395bd1866389c0f3ec157a2ac9a83c0bf
Alex <alexandrucioi@gmail.com>
2022-08-12 17:18:51 +0300

    birou

--- index.js
+++ index.js
@@ -13,6 +13,19 @@ class Cart {
         return this.productList.size;
     }
 
+    unserialize(content) {
+       if (content.hasOwnProperty("livrare")) {
+           const livrare = new Livrare();
+           const adresa = new Address();
+
+           livrare.detalii = content.livrare.detalii;
+           adresa.unserialize(content.livrare.adresa);
+           livrare.adresa = adresa;
+           this.setLivrare(livrare);
+
+       }
+    }
+
     deleteProduct(id) {
         this.productList.delete(id);
     }
@@ -76,6 +89,12 @@ class ProductCatalog {
 
 class Address {
     constructor() { }
+
+    unserialize(adresa) {
+        this.judet = adresa.judet;
+        this.oras = adresa.oras;
+        this.strada = adresa.strada;
+    }
 }
 
 class Livrare {
@@ -89,47 +108,60 @@ class Facturare {
 class Plata {
     constructor() { }
 }
+//localStorage.clear();
+if (localStorage.getItem('cart') == null) {
+    cos = new Cart();
 
-function addProduct(id) {
-    cos.addProduct(id, parseInt(document.getElementById(id + 'Cantitate').value));
-    document.getElementById('numberOfItems').innerHTML = cos.getNumberOfItems();
+    adresaLivrare = new Address();
+    adresaFacturare = new Address();
+    livrare1 = new Livrare();
+    factura1 = new Facturare();
+    plata1 = new Plata();
+    catalog = new ProductCatalog();
 
+    adresaLivrare.judet = 'Dolj';
+    adresaLivrare.oras = 'Craiova';
+    adresaLivrare.strada = 'Sfintii Apostoli';
+
+    livrare1.adresa = adresaLivrare;
+    livrare1.detalii = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.';
+
+    adresaFacturare.judet = 'Bucuresti';
+
+    factura1.cui = '6859662';
+    factura1.adresa = adresaFacturare;
+
+    plata1.tip = 'card';
+    plata1.numarCard = '4469 5350 2910 6378';
+    plata1.lunaExpirare = '08';
+    plata1.anExpirare = '25';
+    plata1.numeDetinator = 'Mihai Alexandru'
+    plata1.cvv = '327';
+
+    cos.setLivrare(livrare1);
+    cos.setFacturare(factura1);
+    cos.setPlata(plata1);
+
+    console.log(cos);
+    localStorage.setItem('cart', JSON.stringify(cos));
+    console.log(JSON.stringify(cos));
+} else {
+    console.log('else');
+    cosContent = JSON.parse(localStorage.getItem('cart'));
+    cos = new Cart();
+    cos.unserialize(cosContent);
     console.log(cos);
 }
 
+function showNumberOfItems() {
 
+}
 
+function addProduct(id) {
 
-cos = new Cart();
-adresaLivrare = new Address();
-adresaFacturare = new Address();
-livrare1 = new Livrare();
-factura1 = new Facturare();
-plata1 = new Plata();
-catalog = new ProductCatalog();
+    cos.addProduct(id, parseInt(document.getElementById(id + 'Cantitate').value));
+    document.getElementById('numberOfItems').innerHTML = cos.getNumberOfItems();
 
-adresaLivrare.judet = 'Dolj';
-adresaLivrare.oras = 'Craiova';
-adresaLivrare.strada = 'Sfintii Apostoli';
+    localStorage.setItem('cart', JSON.stringify(cos));
 
-livrare1.adresa = adresaLivrare;
-livrare1.detalii = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.';
-
-adresaFacturare.judet = 'Bucuresti';
-
-factura1.cui = '6859662';
-factura1.adresa = adresaFacturare;
-
-plata1.tip = 'card';
-plata1.numarCard = '4469 5350 2910 6378';
-plata1.lunaExpirare = '08';
-plata1.anExpirare = '25';
-plata1.numeDetinator = 'Mihai Alexandru'
-plata1.cvv = '327';
-
-cos.setLivrare(livrare1);
-cos.setFacturare(factura1);
-cos.setPlata(plata1);
-
-//console.log(cos);
-//console.log(catalog);
+}
